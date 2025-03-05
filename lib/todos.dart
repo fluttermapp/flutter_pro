07b2472fd200beb9edfbd75e54fb0d273214ed40
fluttermapp <80725968+fluttermapp@users.noreply.github.com>
2025-03-05 15:10:06 -0500

    todos

--- lib/todos.dart
+++ lib/todos.dart
@@ -1,5 +1,4 @@
-//Todo 1: remove dead code, fix unused stateful stateless widgets, remove String and add them in constants, remove constants
-
 //Todo 4: Firebase CRUD, masterclass, auth
 //Todo 5: Clean architecture firebase
 //Todo 6: Clean architecture onboarding
+//Todo 7: Clean architecture change name habit
